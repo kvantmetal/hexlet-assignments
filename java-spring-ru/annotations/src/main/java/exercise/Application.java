89b02791352e6fd5772be39adc369fdf3dc02557
ruavuy3 <yaroslav.vinokurov@raiffeisen.ru>
2024-03-20 21:25:56 +0300

    annotation

--- java-spring-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.lang.reflect.Method;
 
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 
