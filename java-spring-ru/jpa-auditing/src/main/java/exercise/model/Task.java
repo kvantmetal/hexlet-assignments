b8ffd0d48dc98f45109a1c63ea5312e1a9882069
ruavuy3 <yaroslav.vinokurov@raiffeisen.ru>
2024-03-25 13:38:02 +0300

    auditing

---
+++ java-spring-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -0,0 +1,71 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDate;
+
+// BEGIN
+@Entity
+@Table(name = "Task")
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    @CreatedDate
+    private LocalDate createdAt;
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+// END
