5e4344bb52a237e45d5bd5f02e9b286ec79eccc1
ruavuy3 <yaroslav.vinokurov@raiffeisen.ru>
2024-03-28 15:26:21 +0300

    dto update

---
+++ java-spring-ru/dto-to-entity-for-update/src/main/java/exercise/dto/ProductUpdateDTO.java
@@ -0,0 +1,28 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+public class ProductUpdateDTO {
+    private String title;
+    private int price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
+// END
