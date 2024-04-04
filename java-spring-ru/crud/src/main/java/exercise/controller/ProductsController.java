5da3b330c5a84195091c45216836687110119dd1
ruavuy3 <yaroslav.vinokurov@raiffeisen.ru>
2024-04-04 11:24:17 +0300

    crud

---
+++ java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,81 @@
+package exercise.controller;
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
+import exercise.mapper.CategoryMapper;
+import exercise.mapper.ProductMapper;
+import exercise.model.Category;
+import exercise.model.Product;
+import exercise.repository.CategoryRepository;
+import exercise.repository.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    // BEGIN
+    @GetMapping(path = "")
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOS = products.stream().map(p -> productMapper.map(p)).toList();
+        return ResponseEntity.ok(productDTOS);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isEmpty()) {
+            throw new ResourceNotFoundException("Product by id %d not found".formatted(id));
+        }
+
+        Product product = productOptional.get();
+        return ResponseEntity.ok(productMapper.map(product));
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<ProductDTO> saveProduct(@RequestBody @Valid ProductCreateDTO productCreateDTO) {
+        Product product = productMapper.map(productCreateDTO);
+        productRepository.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productMapper.map(product));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ProductDTO> updateProductById(@PathVariable long id, @RequestBody @Valid ProductUpdateDTO productUpdateDTO) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isEmpty()) {
+            throw new ResourceNotFoundException("Product by id %d not found".formatted(id));
+        }
+        Product product = productOptional.get();
+        productMapper.update(productUpdateDTO, product);
+        Optional<Category> byId = categoryRepository.findById(productUpdateDTO.getCategoryId().get());
+        product.setCategory(byId.get());
+        productRepository.save(product);
+        return ResponseEntity.ok(productMapper.map(product));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable long id) {
+        productRepository.deleteById(id);
+    }
+
+    // END
+}
